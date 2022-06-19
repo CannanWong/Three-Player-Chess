f35f81cab7ea4f703293e38d6c8106fe2907f622
Jeshuran Jebanesan <jeshuranjebanesan@dyn3148-5.wlan.ic.ac.uk>
2022-06-19 22:29:43 +0100

    basic files and comment structure set up

---
+++ playchess.c
@@ -0,0 +1,21 @@
+//Game Start
+
+//set up board and place pieces
+
+//is game over
+//if no go to get player
+//if yes end game
+
+//get player
+
+//get playerinput(e.g click)
+
+//show piece valid moves
+
+//move piece
+//if no go to get playerinput
+//if yes go to domove
+
+//do move
+
+//end game
