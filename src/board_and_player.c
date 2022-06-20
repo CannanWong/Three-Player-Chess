bfc4c3c31fa590b0b1ce7eb76c63ea57e21420e3
andy <andy.chen121@imperial.ac.uk>
2022-06-20 21:44:23 +0100

    Add function compare coordinates

--- src/board_and_player.c
+++ src/board_and_player.c
@@ -1,24 +1,100 @@
 #include "chess.h"
 
-piece_t get_piece(coord_t grid) {
-  piece_t *region;
-  switch (grid.region_col) {
-    case (WHITE){
-      region = board.white_region;
+color adjacent(color self, bool prev) {
+  switch (self) {
+    case (BLACK) {
+      if (prev) {
+        return RED;
+      }
+      return WHITE;
     }
-    case (BLACK){
-      region = board.white_region;
+    case (WHITE) {
+      if (prev) {
+        return BLACK;
+      }
+      return RED;
     }
     default {
-      assert(grid.region_col = RED);
-      region = board.white_region;
+      assert (self == RED);
+      if (prev) {
+        return WHITE;
+      }
+      return BLACK;
+    }
+  }
+}
+
+bool coord_equals(coord_t c1, coord_t c2) {
+  return (c1.region == c2.region && c1.x = c2.x && c1.y == c2.y);
+}
+
+piece_t* get_piece(coord_t grid) {
+  piece_t ***reg;
+  switch (grid.region) {
+    case (WHITE){
+      reg = board.white_region;
+    }
+    case (BLACK){
+      reg = board.white_region;
+    }
+    default {
+      assert(grid.region = RED);
+      reg = board.white_region;
     } 
   }
   return board[grid.x][grid.y];
 }
 
-coord_t move_vector(bool l_first, coord_t orig, signed short dx, signed short dy) {
-  if (l-first) {
+coord_t move_x(coord_t orig, signed short dx) {
+  coord_t dest;
+  unsigned short new_x = orig.x + dx;
+  assert(0 <= new_x && new_x < MAX_X);
+  dest.x = new_x;
+  dest.y = orig.y;
+  dest.region = orig.region;
+  return dest;    
+}
+
+coord_t move_y(coord_t orig, signed short dy) {
+  coord_t dest;
+  unsigned short new_y = orig.y + dy;
+  assert(0 <= new_y && new_y < 2 * MAX_Y);
+  if (new_y < MAX_Y) {
+    dest.y = new_y;
+    dest.x = orig.x;
+    dest.region = orig.region;
+  } else {
+    if (orig.x < MAX_X/2) {
+      dest.region = adjacent(orig.region, true);
+    } else {
+      dest.region = adjacent(orig.region, false);
+    }
+    dest.y = 2 * MAX_Y - 1 - new_y;
+    dest.x = MAX_X - 1 - orig.x;
+  }
+  return dest;
+}
+
+coord_t move_vector(bool x_first, coord_t orig, signed short dx, signed short dy) {
+  coord_t dest;
+  if (x-first) {
+    dest = move_y(move_x(orig, dx), dy);
+  } else {
+    coord_t temp = move_y(orig, dy);
+    dest = move_x(temp, -1 * dx);
+  }
+  return dest;
+}
+
+bool click_draw(player_t *pl) {
+  pl->agree_draw = true;
+  num_draw++;
+  return true;
+}
+
+bool move_piece(coord_t orig, coord_t dest) {
+
+  for(int i = 0; show_avail_move(coord_t orig)[i]; i++) {
     
   }
 }
--- src/chess.h
+++ src/chess.h
@@ -83,10 +83,11 @@ extern unsigned short num_draw;
 //Functions
 void initialize();
 piece_t get_piece(coord_t);
+bool coord_equals(coord_t, coord_t);
 coord_t move_vector(bool, coord_t, signed short, signed short);
 coord_t* show_avail_move(coord_t);
 bool move_piece(coord_t, coord_t);
-bool click_draw(color);
+bool click_draw(player_t*);
 void terminate();
 void free_board();
 
