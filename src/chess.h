90fa5dcc7d6d62129bf9374f6c00cbc586f01d2c
Yang,JiaJu <jy2921@ic.ac.uk>
2022-06-20 20:07:07 +0100

    header file update

--- src/chess.h
+++ src/chess.h
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define L_AXIS_LEN 8
-#define N_AXIS_LEN 4 
-#define NUM_PIECE_TYPES 6
-#define L_INIT 'a'
-#define N_INIT 1
+#define MAX_X 8
+#define MAX_Y 4
 
 //Enumerations
 typedef enum {BLACK = 0, WHITE = 1, RED = 2} color;
@@ -13,34 +10,33 @@ typedef enum {PAWN,BISHOP,KNIGHT,KING,QUEEN,ROOK} piece;
 typedef enum {GAME, CHECKMATE = 3, STALEMATE = 2, DRAW = 1} status;
 
 //Type definiations
-typedef struct chess_piece {
-  color piece_col;
+typedef struct chess_piece {  
+  bool single_move;
+  color piece_color;
   piece piece_type;
+  coord_t current_coord;
+  int *(move_vecs[2]);
 } piece_t;
 
-typedef struct piece_number_map {
-  piece_t pc;
-  unsigned short pc_num;
-} pc_num_map;
-
 typedef struct chess_board {
-  piece_t black_region[L_AXIS_LEN][N_AXIS_LEN];
-  piece_t white_region[L_AXIS_LEN][N_AXIS_LEN];
-  piece_t red_region[L_AXIS_LEN][N_AXIS_LEN];
+  piece_t black_region[MAX_X][MAX_Y];
+  piece_t white_region[MAX_X][MAX_Y];
+  piece_t red_region[MAX_X][MAX_Y];
 } board_t;
 
 typedef struct coordinate {
-  color region_col;
-  char letter;
-  unsigned short number;
+  color region;
+  int x;
+  int y
 } coord_t;
 
-typedef struct chess_player {
+//request draw trigger interrupt
+typedef struct chess_player { 
+  bool agree_draw;
   color player_col;
   char *name;
   unsigned short score;
-  pc_num_map possession[NUM_PIECE_TYPES];
-  bool agree_draw;
+  //pc_num_map possession[NUM_PIECE_TYPES];
 } player_t;
 
 typedef struct sequence {
@@ -57,13 +53,16 @@ extern unsigned short num_draw;
 //Functions
 void initialize();
 piece_t get_piece(coord_t);
-<<<<<<< HEAD:src/chess.h
-coord_t move_vector(bool, signed short, signed short);
-=======
 coord_t move_vector(coord_t, signed short, signed short);
->>>>>>> 474a078c5427185b165fb782b6219c5af57897df:chess.h
 coord_t* show_avail_move(coord_t);
 bool move_piece(coord_t, coord_t);
-pc_num_map attack(piece_t);
 bool click_draw(color);
 void terminate();
+
+/*
+typedef struct piece_number_map {
+  piece_t pc;
+  unsigned short pc_num;
+} pc_num_map;
+pc_num_map attack(piece_t);
+*/
